7a50eb2e3aa3e3d7cee0037b00b47334972da962
Ilmar7786 <Ilmar7786@yandex.ru>
2024-04-15 22:37:12 +0300

    feat: add rules eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,63 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx"],
+      },
+    },
+  },
+  plugins: ["react-refresh", "simple-import-sort"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:sonarjs/recommended",
+    "plugin:security/recommended-legacy",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+    "import/named": "off",
+    "react/jsx-props-no-spreading": "off",
+    "no-nested-ternary": "off",
+    "import/prefer-default-export": "off",
+    "react/function-component-definition": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-absolute-path": "off",
+    // "import/no-unresolved": ["error", { ignore: [".svg"] }],
+    "import/no-extraneous-dependencies": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(configEnv => ({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    linterPlugin({
+      include: ["./src/**/*.ts", "./src/**/*.tsx"],
+      linters: [new EsLinter({ configEnv }), new TypeScriptLinter()],
+    }),
+  ],
+}))
