8ad426b21d5eb6de0d78cb333002c4ed47f2beb0
imartynov <imartynov@zuzex.com>
2024-04-19 08:20:02 +0300

    feat: update hook pre-commit and lint-staged and add editorconfig

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,10 +1,8 @@
-const path = require("path")
-
 module.exports = {
   // Type check TypeScript files
   "**/*.(ts|tsx)": () => "npx tsc --noEmit",
 
-  "*.{js,css,ts,tsx,jsx}": [
+  "*.{js,ts,tsx,jsx}": [
     "prettier --write",
     "eslint --fix"
   ]
